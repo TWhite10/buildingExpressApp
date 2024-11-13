3ce684254adcdbc8cfdad07a50bf11d6f09612da
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-11-13 02:48:04 -0500

    navbar commit

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-const express = require("express")
+const express = require("express");
 const app = express()
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+const navbar = 
+` <nav style = ' background-image: linear-gradient(180deg,#c4b6fd, #93acfc); padding:25px;' >
+   <a href='/' style = 'color: black;font-weight: bold; text-decoration:none; padding:5px'>Home</a>  
+   <a href='/random-number'   style = 'color: black; font-weight: bold;text-decoration:none; padding:5px'>Random Number</a> 
+   <a href='/time' style = 'color: black; font-weight: bold; text-decoration:none; padding:5px'>Time</a> 
+
+</nav>`
+
+;
+
+//configure ejs
+app.set("view engine", "ejs");
+app.set("views", "views");
+//routing 
+app.get("/",(req,res)=>{
+   
+    res.render('index',{
+         navbar:navbar
+    });
+   
 });
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
