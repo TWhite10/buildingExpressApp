473d658298d983e4ca32eec97e68db1651b54f79
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-11-13 18:37:23 -0500

    commit

--- server.js
+++ server.js
@@ -11,11 +11,29 @@ app.set("views", "views");
 //routing 
 app.get("/",(req,res)=>{
    
-    res.render('index',{
-         
+    res.render("index",{
+         title:"HomePage",
+         message:" hello hello hello "
+
     });
    
-});
+})
+app.get("/users",(req,res)=>{
+    res.render("users",{
+        title:"UserPage",
+        message:" hello hello user user user "
+
+   });
+} )
+
+app.get("/users/new",(req,res)=>{
+    res.render("/users/new",{
+        title:"HomePage",
+        message:" hello hello new new new "
+
+   });
+
+} )
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
