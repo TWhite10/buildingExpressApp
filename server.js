cf56ba80dc2575e05666257ad4f0b73970b4f011
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-11-13 02:54:28 -0500

    navbar commit

--- server.js
+++ server.js
@@ -3,15 +3,7 @@ const app = express()
 const port = 3000;
 
 
-const navbar = 
-` <nav style = ' background-image: linear-gradient(180deg,#c4b6fd, #93acfc); padding:25px;' >
-   <a href='/' style = 'color: black;font-weight: bold; text-decoration:none; padding:5px'>Home</a>  
-   <a href='/random-number'   style = 'color: black; font-weight: bold;text-decoration:none; padding:5px'>Random Number</a> 
-   <a href='/time' style = 'color: black; font-weight: bold; text-decoration:none; padding:5px'>Time</a> 
 
-</nav>`
-
-;
 
 //configure ejs
 app.set("view engine", "ejs");
@@ -20,7 +12,7 @@ app.set("views", "views");
 app.get("/",(req,res)=>{
    
     res.render('index',{
-         navbar:navbar
+         
     });
    
 });
