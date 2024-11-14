60a3e86e52f74d4fdee4ef9a33da69a485243a58
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-11-14 17:48:11 -0500

    commit

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-const express = require("express");
+import express from "express";
+import path from "path";
+
 const app = express()
 const port = 3000;
 
+const users = [
+    { userid: 1, username: 'Mary Bear' },
+    { userid: 2, username: 'John Trouble' }
+];
+//body parsermiddleware
+app.use(express.json());
 
+app.use(express.urlencoded({ extended: false })); 
 
 
 //configure ejs
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+
 //routing 
 app.get("/",(req,res)=>{
    
     res.render("index",{
          title:"HomePage",
-         message:" hello hello hello "
+         message:" Welcome to the Admin homepage "
 
     });
    
@@ -21,19 +32,42 @@ app.get("/",(req,res)=>{
 app.get("/users",(req,res)=>{
     res.render("users",{
         title:"UserPage",
-        message:" hello hello user user user "
+        message:"This is the current users list",
+        users: users,
 
    });
 } )
 
-app.get("/users/new",(req,res)=>{
-    res.render("/users/new",{
-        title:"HomePage",
-        message:" hello hello new new new "
+app.get("/new-users",(req,res)=>{
+    res.render("new-users",{
+        title:"New User",
+        message:" Hello New User - Please submit your id and name to be added to the system"
 
    });
 
-} )
+} );
+app.post("/users", (req, res) => {
+    const { userid,username } = req.body;
+    //validate userid and username entries 
+    if (!userid || !username){
+        //add alert
+        return res.status(400).send("Please enter UserId and UserName");
+    }
+    //check if username already exists 
+    if (users.some(user => user.userid === parseInt(userid))) {
+         //add alert
+        return res.status(400).send("User ID already exists");
+    }
+    //add new user
+    users.push({ userid: parseInt(userid), username });
+    res.redirect("/users");
+});
+//Middleware function
+const logReq = function(req,res,next){
+    console.log("Request received ")
+    next();
+}
+app.use(logReq);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
